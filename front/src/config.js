ddcf6234ee327771261099d23598ba8eed4f71ba
DAVI CUSTÓDIO DA SILVA <davi.silva328@senaisp.edu.br>
2026-05-14 09:39:49 -0300

    kek2

---
+++ front/src/config.js
@@ -0,0 +1,3 @@
+const apiBaseURL = import.meta.env.VITE_API_URL || "https://seu-backend.vercel.app";
+
+export default apiBaseURL;
--- producaoBack/src/routes/usuariosRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import usuariosController from "../controllers/usuariosController.js";
-
-const router = express.Router();
-
-router.post("/", usuariosController.criarUsuario);
-router.get("/", usuariosController.selecionarUsuario);
-
-export default router;
